37147267c4b8c6f615d9f7312e2a31a2a80aa052
Youssef Anoun <Youssef.Anoun@wifakbanktunisie.onmicrosoft.com>
2026-06-28 23:07:47 +0100

    securityconfig to *

--- src/main/java/com/ecommerce/backend/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/backend/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.List.of("http://localhost:4200", "http://localhost:4201"));
+        configuration.setAllowedOrigins(java.util.List.of("http://localhost:4200", "http://localhost:4201","*"));
         configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
